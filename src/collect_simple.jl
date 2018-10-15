83e43d8d6582050a429f12e933ee0bc1e735c697
ajkeith <keith.andrew.j@gmail.com>
2018-10-14 22:39:42 -0400

    worst-case true dynamics results

--- src/collect_simple.jl
+++ src/collect_simple.jl
@@ -8,7 +8,7 @@ TOL = 1e-6
 # intialize problems
 ip = SimpleIPOMDP(0.8, 0.7, 0.66, 0.85)
 rip = SimpleRIPOMDP(0.8, 0.7, 0.66, 0.85, 0.15)
-tip = SimpleIPOMDP(0.65, 0.55, 0.51, 0.7)
+tip = SimpleIPOMDP(0.65, 0.85, 0.51, 0.7)
 
 # select belief points
 ss = [[0.0, 1.0], [0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6], [0.5, 0.5],
@@ -19,7 +19,7 @@ for i = 1:length(ss)
 end
 
 # intialize solver
-solver = RPBVISolver(beliefpoints = bs, max_iterations = 200)
+solver = RPBVISolver(beliefpoints = bs, max_iterations = 500)
 
 # solve
 @time solip = RPBVI.solve(solver, ip);
@@ -46,11 +46,13 @@ actiondata = DataFrame(Belief = actionind, StdAction = asip,
 @show actiondata
 
 # sim values for nominal and robust solutions for off-nominal case
-ipoff = tip
 ntest = 3
-nreps = 10
+nreps = 50
 nsteps = 100
-psim = RolloutSimulator(max_steps = nsteps)
+# IMPORTANT: need to find a way to have both default and worst sims
+# right now I can only make worst sims (but some important and good results need the default)
+# psim_default = RolloutSimulator(max_steps = nsteps)
+psim_worst = RolloutSimulator(solrip.alphas, max_steps = nsteps)
 simvals = [Vector{Float64}(nreps) for _ in 1:ntest] # simulated values
 simps = [Vector{Float64}(nreps) for _ in 1:ntest] # simulated percent correct
 ves = Array{Float64}(ntest) # expected values
@@ -61,13 +63,15 @@ pms = Array{Float64}(ntest) # mean percent correct
 pss = Array{Float64}(ntest) # std percent correct
 
 rseed = 92378432
+simprob = rip
+psim = psim_worst
 for i in 1:1
     # println("Run ", ceil(Int, i / 2))
     println("Nominal")
     srand(rseed)
     buip, burip, butip = updater(solip), updater(solrip), updater(soltip)
     @showprogress 1 "Simulating nominal value..." for j = 1:nreps
-        sv, sp = simulate(psim, tip, solip, buip)
+        sv, sp = simulate(psim, simprob, solip, burip)
         simvals[i][j] = sv
         simps[i][j] = sp
     end
@@ -78,8 +82,8 @@ for i in 1:1
     pss[i] = std(simps[i])
     println("Robust")
     srand(rseed)
-    @showprogress 1 "Simulating nominal value..." for j = 1:nreps
-        sv, sp = simulate(psim, tip, solrip, burip)
+    @showprogress 1 "Simulating robust value..." for j = 1:nreps
+        sv, sp = simulate(psim, simprob, solrip, buip)
         simvals[i+1][j] = sv
         simps[i+1][j] = sp
     end
@@ -90,8 +94,8 @@ for i in 1:1
     pss[i+1] = std(simps[i+1])
     println("OffNominal")
     srand(rseed)
-    @showprogress 1 "Simulating nominal value..." for j = 1:nreps
-        sv, sp = simulate(psim, tip, soltip, butip)
+    @showprogress 1 "Simulating off-nominal value..." for j = 1:nreps
+        sv, sp = simulate(psim, simprob, soltip, butip)
         simvals[i+2][j] = sv
         simps[i+2][j] = sp
     end
@@ -102,8 +106,8 @@ for i in 1:1
     pss[i+2] = std(simps[i+2])
 end
 
-sname = "assessment"
-sversion = "1.4"
+sname = "simple"
+sversion = "2.1"
 ves = [policyvalue(solip, e1), policyvalue(solrip, e1),
         policyvalue(soltip, e1)]
 rdata = DataFrame(ID = ["Nominal", "Robust","OffNominal"], ExpValue = ves,
@@ -127,6 +131,37 @@ CSV.write(joinpath(path, fnactions), actiondata)
 ########### testing ############
 rng = MersenneTwister(0)
 b = [0.6, 0.4]
+
+dbip = DiscreteBelief(ip, states(ip), b)
+buip = updater(solip)
+dbtip = DiscreteBelief(tip, states(tip), b)
+butip = updater(soltip)
+dbrip = DiscreteBelief(rip, states(rip), b)
+burip = updater(solrip)
 s = :left
+a = :both
+o = :LR
+
+
+dbip = initialize_belief(buip, initial_state_distribution(ip))
+dbrip = initialize_belief(burip, initial_state_distribution(rip))
+
+dbip = DiscreteBelief(ip, states(ip), [0.745, 0.255])
+dbrip = DiscreteBelief(rp, states(rip), [0.745, 0.255])
 a = :single
-generate_sor(rip, b, s, a, rng)
+o = :LL
+
+update(buip, dbip, a, o).b
+update(butip, dbtip, a, o).b
+
+generate_sor(rip, dbip.b, s, a, rng)
+
+sp = :left
+observation(rip, a, sp)
+psample(observation(rip, a, sp)...)
+
+nsteps = 10
+psim = RolloutSimulator(solrip.alphas, max_steps = nsteps)
+simulate(psim, rip, solrip, buip)
+
+generate_sor_worst(rip, dbip.b, s, a, rng, solrip.alphas)
